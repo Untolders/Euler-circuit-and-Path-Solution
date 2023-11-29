8d30a508c0dc41c067288d99176166dbe3588b60
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2023-11-29 16:40:13 +0530

    Create Euler-circuit-and-Path-Solution.cpp

---
+++ Euler-circuit-and-Path-Solution.cpp
@@ -0,0 +1,87 @@
+*******************************************************************************  Question  ****************************************************************************************************************************
+
+An Eulerian Path is a path in graph that visits every edge exactly once and it starts and ends up at different indexes. An Eulerian Circuit is an Eulerian Path which starts and ends on the same vertex. Given an undirected acyclic graph with V nodes, and E edges, with adjacency list adj, return 2 if the graph contains an eulerian circuit, else if the graph contains an eulerian path, return 1, otherwise, return 0.
+
+Example 1:
+
+Input: 
+
+Output: 2
+Explanation: 
+Following is an eulerian circuit in the mentioned graph
+1 -> 2 -> 0 -> 1
+Example 2:
+
+Input: 
+
+Output: 1
+Explanation: 
+Following is an eulerian circuit in the mentioned graph
+1 -> 0 -> 2
+Your Task:
+You don't need to read or print anything. Your task is to complete the function isEulerCircuilt() which takes number of vertices in the graph denoted as V and an adjacency list of graph denoted as adj and returns 2 if the graph contains an eulerian circuit, else if the graph contains an eulerian path, it returns 1, otherwise, it will return 0.
+
+Expected Time Complexity: O(V+E) where E is the number of edges in graph.
+Expected Space Complexity: O(V)
+
+Constraints:
+1 ≤ V, E ≤ 104
+1 ≤ adj[i][j] ≤ V-1
+
+
+
+
+
+**********************************************************************************  Solution  *************************************************************************************************************************
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+	int isEulerCircuit(int V, vector<int>adj[]){
+	    // Code here
+	          int evens =0; 
+
+        int odds =0; 
+
+        for(int i =0;i< V;i++){ 
+
+            if(adj[i].size() & 1) odds++; 
+
+            else evens++; 
+
+        } 
+
+        if(evens == V) return 2; 
+
+        else if (odds <= 2) return 1; 
+
+        else return 0; 
+
+    
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		Solution obj;
+		int ans = obj.isEulerCircuit(V, adj);
+		cout << ans <<"\n";	}
+	return 0;
+}
+// } Driver Code Ends
+
